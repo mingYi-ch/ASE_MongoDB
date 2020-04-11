76ed8f818dc2776d0c1d47cdc3aee23accfd6268
Ming Yi <myi@student.ethz.ch>
2020-04-11 19:39:21 +0200

    change moive to movies in database; add some queries

--- query_test/queries.py
+++ query_test/queries.py
@@ -1,8 +1,15 @@
 from pymongo import MongoClient
 
 client = MongoClient("localhost:27017")
-moives = client.ase.moives
+movies = client.ase.movies 
 
-doc = moives.find_one({'adult': False})
+# test 
+doc = movies.find_one({'adult': False})
 assert(doc['adult'] == False)
     
+# find 20 movies at one time, and return two each time to the client,return only fields we want
+docs = movies.find({'id': 1, 'revenue': 1, 'runtime': 1, 'popularity': 1}).limit(20)
+
+# aggregated, features for ratings
+
+
