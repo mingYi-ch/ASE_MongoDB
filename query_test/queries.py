cbd2ececd307392ea7ef9c771d4bb7340ae66b28
Ming Yi <myi@student.ethz.ch>
2020-04-23 14:34:41 +0200

    update query test

--- query_test/queries.py
+++ query_test/queries.py
@@ -3,12 +3,9 @@ from pymongo import MongoClient
 client = MongoClient("localhost:27017")
 movies = client.ase.movies 
 
-# test 
-doc = movies.find_one({'adult': False})
-assert(doc['adult'] == False)
-    
 # find 20 movies at one time, and return two each time to the client,return only fields we want
 docs = movies.find({'id': 1, 'revenue': 1, 'runtime': 1, 'popularity': 1}).limit(20)
+assert(len(list(docs)) > 0)
 
 # aggregated, features for ratings
 
