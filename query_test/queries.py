bf07f14a49e71e58d47b9b5e1e01c19a626bd2bd
Ming Yi <myi@student.ethz.ch>
2020-04-23 16:45:20 +0200

    update query_test

--- query_test/queries.py
+++ query_test/queries.py
@@ -4,9 +4,9 @@ client = MongoClient("localhost:27017")
 movies = client.ase.movies 
 
 # find 20 movies at one time, and return two each time to the client,return only fields we want
-docs = movies.find({'id': 1, 'revenue': 1, 'runtime': 1, 'popularity': 1}).limit(20)
+docs = movies.find(projection = {'id': 1, 'revenue': 1, 'runtime': 1, 'popularity': 1}).limit(20)
 assert(len(list(docs)) > 0)
-
+# print(list(docs))
 # aggregated, features for ratings
 
 
